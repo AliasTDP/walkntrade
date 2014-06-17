5fde47cc450b4b59da29bd1f792a340c46a5bba1
Tevin Phillip <alias.tdp@gmail.com>
2014-06-17 00:37:14 -0400

    selector is no longer referred to as selector2.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once "framework/CredentialStore.php";
 $cs = new CredentialStore();
 $schoolTextId = $cs->cookieCheck("sPref");
 if($cs->getSchoolName($schoolTextId) == null){
-	include("selector2.php");
+	include("selector.php");
 }
 else{
 	header('Location: /schools/'.$schoolTextId);
