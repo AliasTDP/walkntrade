852da85a73c7f0e33708ddeb9df956590c43eb01
Tevin Phillip <alias.tdp@gmail.com>
2014-06-17 18:52:42 -0400

    Revert "selector is no longer referred to as selector2.php"
    This reverts commit 5fde47cc450b4b59da29bd1f792a340c46a5bba1.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once "framework/CredentialStore.php";
 $cs = new CredentialStore();
 $schoolTextId = $cs->cookieCheck("sPref");
 if($cs->getSchoolName($schoolTextId) == null){
-	include("selector.php");
+	include("selector2.php");
 }
 else{
 	header('Location: /schools/'.$schoolTextId);
