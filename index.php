53534795981cb7dba6b8376e790f9cc284ce5e67
Tevin <alias.tdp@gmail.com>
2014-11-13 19:44:36 -0500

    Made some changes to  user agent

--- index.php
+++ index.php
@@ -4,17 +4,6 @@ require_once "framework/CredentialStore.php";
 $cs = new CredentialStore();
 $schoolTextId = $cs->cookieCheck("sPref");
 if($cs->getSchoolName($schoolTextId) == null){
-	//if mobile
-	//redirect to mobile selector page
-	$needles = array("Android", "iPhone", "iPad", "Windows Phone");
-	foreach ($needles as $needle) {
-		if(strpos($_SERVER["HTTP_USER_AGENT"], $needle) !== false){
-			include("mobile/index.html");
-			return;
-		}
-	}
-	//otherwise
-	//include normal selector
 	include("selector.php");
 }
 else{
