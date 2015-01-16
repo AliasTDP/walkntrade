d053af7d7609d9feead0e77f4038a3277f1f6a3b
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2015-01-16 01:50:54 -0500

    Condense index.php a bit

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <?php
+require_once "framework2/CredentialStore.php";
+
 $serverName = basename($_SERVER["SERVER_NAME"]);
 $serverURI = $_SERVER['REQUEST_URI'];
 $validDomains = array("walkntrade.com", "dev.wt", "50.147.246.201", "172.16.10.71");
-$redirect = true;
-foreach ($validDomains as $domain) {
-	if($serverName === $domain){
-		$redirect=false;
-	}
-}
-if($redirect)
-	header( 'Location: https://walkntrade.com'.$serverURI, true, 301 );
 
-$query = (isset($_GET["query"])) ? $_GET["query"] : "";
-require_once "framework2/CredentialStore.php";
+if (!in_array($serverName, $validDomains, true)) {
+    header('Location: https://walkntrade.com'.$serverURI, true, 301);
+}
+
 $cs = new CredentialStore();
 $schoolTextId = $cs->cookieCheck("sPref");
-if($cs->getSchoolName($schoolTextId) == null){
-	include("selector.php");
+if ($cs->getSchoolName($schoolTextId) === null) {
+    include("selector.php");
+} else {
+    $query = (isset($_GET["query"])) ? $_GET["query"] : "";
+    header('Location: /schools/'.$schoolTextId.'?query='.$query);
 }
-else{
-	header('Location: /schools/'.$schoolTextId.'?query='.$query);
-}
-
 ?>
