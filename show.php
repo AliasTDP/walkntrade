01829e8b4de3ba1ac4a99f90bdaebed84c3d8a10
Tevin <alias.tdp@gmail.com>
2015-01-21 18:24:08 -0500

    localized validDomains to Walkntradae class

--- show.php
+++ show.php
@@ -11,9 +11,8 @@ $myPost = false;
 
 $serverName = basename($_SERVER["SERVER_NAME"]);
 $serverURI = $_SERVER['REQUEST_URI'];
-$validDomains = array("walkntrade.com", "dev.wt", "50.147.246.201", "172.16.10.71");
 $redirect = true;
-foreach ($validDomains as $domain) {
+foreach ($cs->getValidDomains() as $domain) {
 	if($serverName === $domain){
 		$redirect=false;
 	}
